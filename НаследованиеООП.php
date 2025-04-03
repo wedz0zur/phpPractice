ea86e2994c948c92ed741660efe1687c72245578
Zurab <101800477+wedz0zur@users.noreply.github.com>
2025-04-03 09:43:24 +0300

    Update НаследованиеООП.php

--- НаследованиеООП.php
+++ НаследованиеООП.php
@@ -13,6 +13,11 @@ class Person
         $this->address = $address;
 
     }
+    // final function getInfo()
+    function getInfo()
+    {
+        echo "id: " . $this->id . "<br> Имя: " . $this->name . "<br> Почта: " . $this->email . "<br> Номер: " . $this->phone;
+    }
 }
 
 
@@ -20,12 +25,26 @@ class Employee extends Person
 {
 
     private $company;
+    function __construct($id, $name, $email, $phone, $address, $company)
+    {
+        $this->company = $company;
+        // parent::__construct($id, $name, $email, $phone, $address);
+        Person::__construct($id, $name, $email, $phone, $address);
+        // $this->name = $name;
+        // $this->email = $email;
+        // $this->phone = $phone;
+        // $this->address = $address;
+    }
+
     function getInfo()
     {
-        echo "id: " . $this->id . "<br> Имя: " . $this->name . "<br> Почта: " . $this->email . "<br> Номер: " . $this->phone . "<br> Адресс: " . $this->address;
+        return "<br> <br>id: " . $this->id . "<br> Имя: " . $this->name . "<br> Почта: " . $this->email . "<br> Номер: " . $this->phone . "<br> Адресс: " . $this->address . "<br> Компания: " . $this->company . "<br>" . parent::getInfo();
     }
 }
 
-$employ = new Employee("06", "Zurab", "wedzigew777@gmail.com", "+79225663407", "Plievo");
-$employ->getInfo();
+$employ = new Employee("06", "Zurab", "vedzigev777@gmail.com", "+79283458723", "Plievo", "Яндекс");
+echo $employ->getInfo();
+
+
+echo $employ instanceof Employee;
 ?>
