9546e96b3ec3c7d57404914929cfbe8d70a3e9fe
Zurab <101800477+wedz0zur@users.noreply.github.com>
2025-04-17 10:26:05 +0300

    Update Модификаторы доступа.php

--- Модификаторы доступа.php
+++ Модификаторы доступа.php
@@ -5,7 +5,8 @@ class Person
     public $publicA = "Доспутен всем";
     protected $protectedA = "Доступен наследнику и родителю";
 
-    function getInfo(){
+    function getInfo()
+    {
         return $this->privateA;
     }
 
@@ -31,5 +32,32 @@ class Person
 }
 
 Person::getInformation();
+
+
+class User
+{
+    static $id = 0;
+    public $name;
+    public $age;
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        User::$id++;
+    }
+}
+$user1 = new User("Мага", 29);
+echo  "id: " . User::$id . " ";
+echo "Имя: " . $user1->name . " " . $user1->age . "<br>";
+
+
+$user2 = new User("Иван", 42);
+echo "id: " . User::$id . " ";
+echo "Имя: " . $user2->name . " " . $user2->age . "<br>";
+
+
+$user3 = new User("Чанга", 12);
+echo "id: " . User::$id . " ";
+echo "Имя: " . $user3->name . " " . $user3->age . "<br>";
 
 ?>
